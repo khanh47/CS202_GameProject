d432cc95777ae62bd57dc612dcf3a69f1514cd12
thinh-nguyen-007 <thinhnguyenctg2007@gmail.com>
2026-07-18 15:11:14 +0700

    Update Save/Load Game

---
+++ include/Game/Behaviours/SaveLoadGame/SaveLoadGame.h
@@ -0,0 +1,22 @@
+#include <filesystem>
+#include <vector>
+
+#pragma once
+
+using namespace std;
+
+class SaveLoadGame {
+private:
+    vector <string> saveFiles;
+    filesystem::path saveFilePath = "assets/SaveGameFiles";
+public:
+    SaveLoadGame();
+    virtual ~SaveLoadGame() = default;
+
+    string GetFile(int index);
+    void SaveGame(const string& saveFileName, const string& gameState);
+    void LoadGame(const string& saveFileName, string& gameState);
+    string operator [](int index) {
+        return saveFiles[index];
+    }
+};
