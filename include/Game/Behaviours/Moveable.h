d3041c69a67985c388fd658f26caeb9a859846a4
khanh47 <phhkhanh47@gmail.com>
2026-07-28 02:53:03 +0700

    fix: remove facing right

--- include/Game/Behaviours/Moveable.h
+++ include/Game/Behaviours/Moveable.h
@@ -2,9 +2,9 @@
 
 class Moveable {
 public:
-    void startMoveLeft() { _movingLeft = true; _facingLeft = true; _facingRight = false; }
+    void startMoveLeft() { _movingLeft = true; _facingLeft = true; }
     void stopMoveLeft() { _movingLeft = false; }
-    void startMoveRight() { _movingRight = true; _facingLeft = false; _facingRight = true; }
+    void startMoveRight() { _movingRight = true; _facingLeft = false; }
     void stopMoveRight() { _movingRight = false; } 
     void startJump() { _jumping = true; }
     void stopJump() { _jumping = false; }
@@ -12,12 +12,10 @@ public:
     bool isMovingRight() const { return _movingRight; }
     bool isMovingLeft() const { return _movingLeft; }
     bool isFacingLeft() const { return _facingLeft; }
-    bool isFacingRight() const { return _facingRight; }
     bool isJumping() const { return _jumping; }
 
 private:
     bool _facingLeft = false;
-    bool _facingRight = false;
     bool _movingLeft = false;
     bool _movingRight = false;
     bool _jumping = false;
