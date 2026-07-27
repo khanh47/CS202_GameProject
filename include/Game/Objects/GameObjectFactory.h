26b3d4c8d4c19606ceb61a2455699ce3be29eb92
khanh47 <phhkhanh47@gmail.com>
2026-07-28 02:42:52 +0700

    fix: implement template for a cleaner constructor

--- include/Game/Objects/GameObjectFactory.h
+++ include/Game/Objects/GameObjectFactory.h
@@ -3,33 +3,43 @@
 #include <functional>
 #include <memory>
 #include <string>
-#include <vector>
 #include <unordered_map>
 
 #include <SFML/Graphics.hpp>
 
 #include "Game/Objects/GameObject.h"
-#include "Physics/PhysicsWorld.h"
 
 class GameObjectFactory {
 public:
     using Creator = std::function<std::shared_ptr<GameObject>(sf::Texture* texture)>;
-    using PlayerCreator = std::function<std::shared_ptr<GameObject>(sf::Texture* texture, const std::string& animationSetId)>;
+    using AnimatedCreator = std::function<std::shared_ptr<GameObject>(sf::Texture* texture, const std::string& animationSetId)>;
+
+    template<typename T>
+    static std::shared_ptr<GameObject> createAnimated(sf::Texture* texture, const std::string& animId) {
+        if (texture) return std::make_shared<T>(*texture, animId);
+        return std::make_shared<T>();
+    }
+
+    template<typename T>
+    static std::shared_ptr<GameObject> createStatic(sf::Texture* texture) {
+        if (texture) return std::make_shared<T>(*texture);
+        return std::make_shared<T>();
+    }
 
     GameObjectFactory();
 
-    void registerPlayer(const std::string& key, PlayerCreator creator);
+    void registerPlayer(const std::string& key, AnimatedCreator creator);
     void registerBlock(const std::string& key, Creator creator);
-    void registerEnemy(const std::string& key, PlayerCreator creator);
+    void registerEnemy(const std::string& key, AnimatedCreator creator);
     void registerItem(const std::string& key, Creator creator);
 
     std::shared_ptr<GameObject> createPlayer(const std::string& key = "Player", sf::Texture* texture = nullptr, const std::string& animationSetId = "mario") const;
     std::shared_ptr<GameObject> createBlock(const std::string& key = "Block", sf::Texture* texture = nullptr) const;
-    std::shared_ptr<GameObject> createEnemy(const std::string& key = "Enemy", sf::Texture* texture = nullptr, const std::string& animationSetId = "goomba") const;
+    std::shared_ptr<GameObject> createEnemy(const std::string& key, sf::Texture* texture = nullptr, const std::string& animationSetId = "") const;
     std::shared_ptr<GameObject> createItem(const std::string& key = "Item", sf::Texture* texture = nullptr) const;
 private:
-    std::unordered_map<std::string, PlayerCreator> _playerCreators;
+    std::unordered_map<std::string, AnimatedCreator> _playerCreators;
     std::unordered_map<std::string, Creator> _blockCreators;
-    std::unordered_map<std::string, PlayerCreator> _enemyCreators;
+    std::unordered_map<std::string, AnimatedCreator> _enemyCreators;
     std::unordered_map<std::string, Creator> _itemCreators;
 };
--- src/Game/Objects/GameObjectFactory.cpp
+++ src/Game/Objects/GameObjectFactory.cpp
@@ -2,53 +2,20 @@
 
 #include "Game/Objects/GameObjectFactory.h"
 #include "Game/Objects/Block/Block.h"
-#include "Game/Objects/Enemy/Enemy.h"
+#include "Game/Objects/Enemy/ConcreteEnemy/Goomba.h"
+#include "Game/Objects/Enemy/ConcreteEnemy/Koopa.h"
 #include "Game/Objects/Item/Item.h"
 #include "Game/Objects/Player/Player.h"
 
 GameObjectFactory::GameObjectFactory() {
-    registerPlayer("Player", [this](sf::Texture* texture, const std::string& animationSetId) -> std::shared_ptr<GameObject> {
-        std::shared_ptr<Player> returned_player;
-        if (texture) {
-            returned_player = std::make_shared<Player>(*texture, animationSetId);
-        } else {
-            returned_player = std::make_shared<Player>();
-        }
-        return returned_player;
-    });
-
-    registerBlock("Block", [this](sf::Texture* texture) -> std::shared_ptr<GameObject> {
-        std::shared_ptr<Block> returned_block;
-        if (texture) {
-            returned_block = std::make_shared<Block>(*texture);
-        } else {
-            returned_block = std::make_shared<Block>();
-        }
-        return returned_block;
-    });
-
-    registerEnemy("Enemy", [this](sf::Texture* texture, const std::string& animationSetId) -> std::shared_ptr<GameObject> {
-        std::shared_ptr<Enemy> returned_enemy;
-        if (texture) {
-            returned_enemy = std::make_shared<Enemy>(*texture, animationSetId);
-        } else {
-            returned_enemy = std::make_shared<Enemy>();
-        }
-        return returned_enemy;
-    });
-
-    registerItem("Item", [this](sf::Texture* texture) -> std::shared_ptr<GameObject> {
-        std::shared_ptr<Item> returned_item;
-        if (texture) {
-            returned_item = std::make_shared<Item>(*texture);
-        } else {
-            returned_item = std::make_shared<Item>();
-        }
-        return returned_item;
-    });
+    registerPlayer("Player", createAnimated<Player>);
+    registerBlock("Block", createStatic<Block>);
+    registerEnemy("Goomba", createAnimated<Goomba>);
+    registerEnemy("Koopa", createAnimated<Koopa>);
+    registerItem("Item", createStatic<Item>);
 }
 
-void GameObjectFactory::registerPlayer(const std::string& key, PlayerCreator creator) {
+void GameObjectFactory::registerPlayer(const std::string& key, AnimatedCreator creator) {
     _playerCreators[key] = std::move(creator);
 }
 
@@ -56,7 +23,7 @@ void GameObjectFactory::registerBlock(const std::string& key, Creator creator) {
     _blockCreators[key] = std::move(creator);
 }
 
-void GameObjectFactory::registerEnemy(const std::string& key, PlayerCreator creator) {
+void GameObjectFactory::registerEnemy(const std::string& key, AnimatedCreator creator) {
     _enemyCreators[key] = std::move(creator);
 }
 
